43c8ee9e886fb0d148432a5bd27bec112a88e469
Diego Anderson <dieanderson@gmail.com>
2022-02-16 21:44:30 -0300

    api post users + password crypto

---
+++ pages/api/users.js
@@ -0,0 +1,38 @@
+import dbConnect from '../../src/utils/dbconnect'
+import { crypto } from '../../src/utils/password'
+import UsersModel from '../../src/models/users'
+
+const users = async (req, res) => {
+    const { method } = req
+
+    switch(method) {
+        case 'GET':
+            await dbConnect()
+            res.status(200).json({ success: true })
+        break
+
+        case 'POST':
+            const { 
+                name, 
+                email, 
+                password, 
+            } = req.body
+
+            await dbConnect()
+
+            const passwordCrypto = await crypto(password)
+
+            const user = new UsersModel({ 
+                name, 
+                email, 
+                password: passwordCrypto, 
+            })
+            
+            user.save()
+            
+            res.status(201).json({ success: true })
+        break
+    }
+}
+
+export default users
